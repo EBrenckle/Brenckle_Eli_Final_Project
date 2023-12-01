dc730bfa47fcaaca0a004048a28d353a05737040
EBrenckle <142259226+EBrenckle@users.noreply.github.com>
2023-12-01 12:47:39 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# Created by Eli Brenckle
+# Sources
+
+# Idea: Metroidvania using Unity
+# Goals: Basic animations (stick figures), use of movement powerups, two or more bosses, handful of different enemies. 
+# Movement: Double jump, Wall jump, Dash, Permeation, etc.
+# Tools: Melee weapon (with upgrades), Projectile, Heal function. (similar to Hollow Knight)
+# Since I am not planning on using python and will be focusing on using Unity(C#) I am making the project more involved.
