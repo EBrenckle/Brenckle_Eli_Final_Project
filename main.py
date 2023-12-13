2f3937ee93d6e5f3da81dbed4ff83a099e474d3a
EBrenckle <Eli.brenckle25@bcp.org>
2023-12-13 13:51:58 -0800

    Idea Update

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # Created by Eli Brenckle
 # Sources
 
-# Idea: Metroidvania using Unity
-# Goals: Basic animations (stick figures), use of movement powerups, two or more bosses, handful of different enemies. 
-# Movement: Double jump, Wall jump, Dash, Permeation, etc.
-# Tools: Melee weapon (with upgrades), Projectile, Heal function. (similar to Hollow Knight)
+# Idea: "Path of Pain" style platformer using Unity
+# Goals: Basic animations (stick figures), use of movement powerups, multiple levels.
+# Movement: Double jump, Wall jump, Dash, Permeation.
+# Tools: Check point function, pogo resets & heal function. (similar to Hollow Knight)
 # Since I am not planning on using python and will be focusing on using Unity(C#) I am making the project more involved.
-#hey
